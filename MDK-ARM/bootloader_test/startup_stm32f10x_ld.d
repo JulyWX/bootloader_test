3234bdffa64bdaafa82366e9fed7738a9fa66552
DESKTOP-4A6NGFG\July <1397465332@qq.com>
2022-03-26 15:58:05 +0800

    [changelog][all]代码分层

--- MDK-ARM/bootloader_test/startup_stm32f10x_ld.d
+++
@@ -1,1 +0,0 @@
-bootloader_test\startup_stm32f10x_ld.o: ..\Drivers\CMSIS\Include\startup_stm32f10x_ld.s
---
+++ MyProject/Project_APP/MDK-ARM/Project_APP/startup_stm32f10x_ld.d
@@ -0,0 +1,1 @@
+project_app\startup_stm32f10x_ld.o: ..\..\bootloader_test\Drivers\CMSIS\Include\startup_stm32f10x_ld.s
